c8fa7946bcef4d90650ee6d729b0302d06884551
Рылеев Денис <ultimate.pelemewka@gmail.com>
2019-03-28 00:13:23 +0300

    Добавьте файлы проекта.

---
+++ Техникум/Controllers/PaginatedList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Техникум.Models;
+
+namespace техникум.Controllers
+{
+    internal class PaginatedList<T>
+    {
+        internal static Task<object> CreateAsync(IQueryable<Student> queryable, int v, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
